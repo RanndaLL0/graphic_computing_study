87632306b3259cc5313ecc0ac572d47202b31974
FELIPE RAMOS VICTOR <felipevictor5120@gmail.com>
2025-08-20 21:04:51 -0300

    subindo o glfw e glew

--- Aula/src/source.cpp
+++ Aula/src/source.cpp
@@ -3,6 +3,94 @@
 #include <GLFW\glfw3.h>
 
 const GLint WIDTH = 1024, HEIGHT = 728;
+GLuint VAO, VBO, shaderProgram;
+
+// linguagem GLSL (O vertexShader recebe o x e y para posicionar o triângulo em tela)
+// layout(location = 0) in vec2 pos, passo como argumento para o programa um vetor;
+// gl_Position recebe um parametro de vetor de 4 variaveis (x,y,z,w)
+static const char* vertexShader = "							\n\
+#version 330												\n\
+															\n\
+layout(location=0) in vec2 pos;								\n\
+															\n\
+void main() {												\n\
+	gl_Position() = vec4(pos.x,pos.y,0.0,1.0)				\n\
+}															\n\
+															\n\
+															\n\
+";
+
+//Podemos entender um ponto como sendo um fragmento de uma poligono ou figura geométrica
+//iremos definir a cor da nossa figura geométrica
+//uma vez que sua cor possa ser alterada pela luz do programa
+static const char* fragmentShader = "						\n\
+#version 330												\n\
+															\n\
+uniform in vec3 triColor;									\n\
+															\n\
+void main() {												\n\
+	color = vec4(triColor,1.0)								\n\
+}															\n\
+															\n\
+															\n\
+";
+
+void createTriangle() {
+	GLfloat vertices[] = {
+		0.0f, 1.0f,
+		-1.0f, -1.0f,
+		1.0f,-1.0f
+	};
+
+	//ira armazenar o vertifice em memória de video, ira ficar na posição 0 (que não é a posição em memoria ram)
+	glGenVertexArrays(1, &VAO);
+	//posibilita a alteração do valor em espaço de memória apontando para o endereço
+	glBindVertexArray(VAO);
+		
+	//aloca dentro do VAO o VBO
+	glGenBuffers(1, &VBO);
+	//aponta para o VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//sinaliza como as informações do vertice estão dispostas
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glEnableVertexAttribArray(0);
+
+	//deixa de apontar para o VAO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+}
+
+//adiciona um programa em execução
+void add_triangle(GLuint program, const char* shaderCode, GLenum type) {
+	GLuint _shader = glCreateShader(type);
+	
+	//Converte char para GLchar
+	const GLchar* code[1];
+	code[0] = shaderCode;
+
+	glShaderSource(_shader, 1, code, NULL);
+	glCompileShader(_shader);
+
+	//tratar os erros
+	
+	glAttachShader(program, _shader);
+}
+
+void add_program() {
+	shaderProgram = glCreateProgram();
+	if (!shaderProgram) {
+		printf("Erro ao criar o programa");
+		return;
+	}
+	add_triangle(shaderProgram, vertexShader,GL_VERTEX_SHADER);
+	add_triangle(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+
+	glLinkProgram(shaderProgram);
+}
 
 int main() {
 	// iniciar o glfw
@@ -41,7 +129,6 @@ int main() {
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
 		printf("Erro ao iniciar o GLEW");
-
 		glfwDestroyWindow(window);
 		glfwTerminate();
 		return 1;
