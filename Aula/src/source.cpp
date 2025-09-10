044eb69cdc44f48ecd876103ecb4fb92cb590113
FELIPE RAMOS VICTOR <felipe.rvictor@senacsp.edu.br>
2025-09-10 20:55:30 -0300

    subindo a aula 6

--- Aula/src/source.cpp
+++ Aula/src/source.cpp
@@ -1,166 +1,257 @@
-#include <stdio.h>
-#include <cstdlib>
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
-#include <time.h>
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW//glfw3.h>
+#include <cstdlib>			// For rand() and srand()
+#include <ctime>			// For time()
+#include <time.h>			// * time_t, struct tm, difftime, time, mktime */
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
-const GLint WIDTH = 1024, HEIGHT = 728;
-GLuint VAO, VBO, shaderProgram;
+using namespace std;
 
-// linguagem GLSL (O vertexShader recebe o x e y para posicionar o triângulo em tela)
-// layout(location = 0) in vec2 pos, passo como argumento para o programa um vetor;
-// gl_Position recebe um parametro de vetor de 4 variaveis (x,y,z,w)
-static const char* vertexShader = "							\n\
-#version 330												\n\
-															\n\
-layout(location=0) in vec2 pos;								\n\
-															\n\
-void main() {												\n\
-	gl_Position = vec4(pos.x,pos.y,0.0,1.0);				\n\
-}															\n\
-															\n\
-															\n\
+const GLint WIDTH = 800, HEIGHT = 600;
+// VAO é o ponteiro do programa que estou rodando, espaço de memória que representa aquele objeto
+// VBO é o ponteiro para os meus dados brutos, são os dados do objeto a ser representado pelo VAO 
+// shaderProgram é qual programa estou rodando 
+// todo programa pode ser chamado de shader
+GLuint VAO, VBO, IBO, shaderProgram;
+
+float toRadians = 3.1415 / 180.0f;
+
+bool direction = false, up = false, directionSize = false;
+float triOffSetX = 0.0f, triOffSetMax = 0.7f, triOffSetMin = -0.7f, triIncrementX = 0.01f;
+float triOffSetY = 0.0f, triIncrementY = 0.01781f;
+float triOffSetSize = 0.2f, triOffSetSizeMax = 0.8f, triOffSetSizeMin = 0.2f, triOffSetSizeIncriment = 0.01f;
+float triCurrentAngle = 0.0f, triAngleIncriment = 1.0f;
+
+
+// aqui estamos fazendo um programa (shader) em GLSL
+
+// shader para renderizar pontos na tela
+static const char* vertexShader = "                                                        \n\
+#version 330                                                                               \n\
+                                                                                           \n\
+                                                                                           \n\
+// passando um argumento para o inicio do programa (args do C//                            \n\
+// estou passando um argumento de entrada na primeira posiçâo                              \n\
+// esse argumento deve ser um vetor de duas posições                                       \n\
+layout(location=0) in vec3 pos;                                                            \n\
+uniform mat4 model;                                                                        \n\
+uniform mat4 projection;																   \n\
+out vec4 vCol;																			   \n\
+                                                                                           \n\
+void main() {                                                                              \n\
+	gl_Position = projection * model * vec4(pos, 1.0f);									   \n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);											   \n\
+}                                                                                          \n\
 ";
 
-//Podemos entender um ponto como sendo um fragmento de uma poligono ou figura geométrica
-//iremos definir a cor da nossa figura geométrica
-//uma vez que sua cor possa ser alterada pela luz do programa
-//tricolor ira ser passado como argumento para dentro do programa
-//assim ele ira ser execultado quando alterarmos a cor do objeto
-static const char* fragmentShader = "						\n\
-#version 330												\n\
-															\n\
-uniform vec3 triColor;										\n\
-out vec4 color;												\n\
-															\n\
-void main() {												\n\
-	color = vec4(triColor,1.0);								\n\
-}															\n\
-															\n\
-															\n\
+// fragment pode ser lido como "a partir desse ponto o que faço com ele?"
+// shader para atribuir cores aos pontos
+
+static const char* fragmentShader = "                                                      \n\
+#version 330                                                                               \n\
+                                                                                           \n\
+// diferente da entrada por layout, uniform é uma entrada em tempo de execução             \n\
+in vec4 vCol;																			   \n\
+uniform vec3 triColor;																	   \n\
+out vec4 color;																			   \n\
+                                                                                           \n\
+void main() {                                                                              \n\
+	color = vCol;			                                                               \n\
+}                                                                                          \n\
 ";
 
-void createTriangle() {
-	GLfloat vertices[] = {
-		0.0f, 1.0f,
-		-1.0f, -1.0f,
-		1.0f,-1.0f
+void create_triangle() {
+
+	//interligando os pontos dos triângulos
+	//esta lista de indices é chamada de IBO (indice buffer object)
+	unsigned int indices[] = {
+		0,1,2, // BASE
+		0,1,3, // Esquerda
+		0,2,3, // Direita
+		1,2,3  // Frente
 	};
 
-	//ira armazenar o vertifice em memória de video, ira ficar na posição 0 (que não é a posição em memoria ram)
+
+	GLfloat vertices[] = { // nosso buffer de vertíces
+		0.0f, 1.0f, 0.0f, // vertice 1 
+		-1.0f, -1.0f, 0.0f, // vertice 2
+		1.0f, -1.0f, 0.0f, // vertice 3
+		0.0f, 0.0f, 1.0f // vertice 4
+	};
+
+	// iniciar um VAO
+	// vamos abrir um espaço de memória na placa de vídeo
+	// pegamos o endereço virutal da placa de video e guardamos ele na variável lá em cima
 	glGenVertexArrays(1, &VAO);
-	//posibilita a alteração do valor em espaço de memória apontando para o endereço
+
+	// estamos dizendo que a partir desse momento todas as alterações serão feitas nesse espaço de memória
+	// agora tudo o que vamos criar será nesse espaço de memória como os pontos da nossa tela
 	glBindVertexArray(VAO);
-	
-	//aloca dentro do VAO o VBO
-	glGenBuffers(1, &VBO);
-	//aponta para o VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//sinaliza como as informações do vertice estão dispostas
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-	glEnableVertexAttribArray(0);
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	//deixa de apontar para o VAO
+	glGenBuffers(1, &VBO); // alocar um buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // a partir desse momento estamos modificando esse buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // explicando onde estão os dados
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // explicando como interpretar esses dados
+	glEnableVertexAttribArray(0);  // location
+
+	// deixar de apontar para o VAO e VBO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
 }
 
-//adiciona um programa em execução
-void add_triangle(GLuint program, const char* shaderCode, GLenum type) {
+// função para executar um buffer na tela
+void add_shader(GLuint program, const char* shaderCode, GLenum type) {
 	GLuint _shader = glCreateShader(type);
-	
-	//Converte char para GLchar
+
+	// converte char para glchar
 	const GLchar* code[1];
 	code[0] = shaderCode;
-
 	glShaderSource(_shader, 1, code, NULL);
 	glCompileShader(_shader);
 
-	//tratar os erros
-	
+	// tratar os erros
+
 	glAttachShader(program, _shader);
 }
 
 void add_program() {
 	shaderProgram = glCreateProgram();
 	if (!shaderProgram) {
-		printf("Erro ao criar o programa");
-		return;
+		cout << "Erro: falha ao criar o programa!";
 	}
 
-	add_triangle(shaderProgram, vertexShader,GL_VERTEX_SHADER);
-	add_triangle(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+	add_shader(shaderProgram, vertexShader, GL_VERTEX_SHADER);
+	add_shader(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+
 	glLinkProgram(shaderProgram);
 }
 
+
 int main() {
-	// iniciar o glfw
+	// inicando GLFW
 	if (!glfwInit()) {
-		printf("Erro ao inicializar o GLFW");
+		cout << "Erro: GLFW não foi iniciado";
 		glfwTerminate();
-		return 1;
+		return -1;
 	}
 
-	//configurações para a inicialização da janela
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// set nas coisas obrigatórias para subir uma janela
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // versão máxima permitida 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // versão mínima
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // somente as funções core
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // qual a precisão de ponto flutuante que vamos usar, precisão da placa
 
-	// Core profile -- extensões basicas para o opengl
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//O glew ira utilizar da arquitetura disponivel da placa de video OBS: GL -> glew.
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "PogersGL", NULL,NULL);
-
+	// criação de janela 
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", NULL, NULL);
 	if (!window) {
-		printf("Erro ao criar a janela");
+		cout << "Erro: Não foi possível criar a janela";
 		glfwTerminate();
-		return 1;
+		return -1;
 	}
-	
-	//armazena em buffer o tamanho real da janela
+
+	// guardando o tamanho real da minha tela 
 	int bufferWidth, bufferHeight;
 	glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
 
-	//trás para o contexto a janela que iremos exibir
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window); // tornando essa janela como principal
 
-	//iniciando o glew
+	//iniciando o Glew
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
-		printf("Erro ao iniciar o GLEW");
+		cout << "Erro: não foi possível iniciar o glew";
 		glfwDestroyWindow(window);
 		glfwTerminate();
-		return 1;
+		return -1;
 	}
 
-	glViewport(0,0,bufferWidth,bufferHeight);
+	glViewport(0, 0, bufferWidth, bufferHeight);
 
-	createTriangle();
+	time_t timer;
+
+	create_triangle();
 	add_program();
+
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth/(GLfloat)bufferHeight, 0.1f, 100.0f);
 
 	while (!glfwWindowShouldClose(window)) {
 
-		//Cor de fundo da janela
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		// cor de fundo da janela
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
-		float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		float g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		float b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 
-		GLint uniformColor = glGetUniformLocation(shaderProgram, "triColor");
-		glUniform3f(uniformColor, r, g, b);
+		// alterar a cor do triangulo
+		GLint uniform_color = glGetUniformLocation(shaderProgram, "triColor");
+		// float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		// float g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		// float b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		glUniform3f(uniform_color, 1.0f, 1.0f, 0.0f);
 
-		//Desenhando o triangulo
-		glUseProgram(shaderProgram);
+		// movimenta o triangulo
+		if (!direction)
+			triOffSetX += triIncrementX;
+		else
+			triOffSetX -= triIncrementX;
+
+		if (!up)
+			triOffSetY -= triIncrementY;
+		else
+			triOffSetY += triIncrementY;
+
+		if (triOffSetX > triOffSetMax || triOffSetX < triOffSetMin)
+			direction = !direction;
+
+		if (triOffSetY > triOffSetMax || triOffSetY < triOffSetMin)
+			up = !up;
+
+		triCurrentAngle += triAngleIncriment;
+		if (triCurrentAngle >= 360)
+			triCurrentAngle = 0;
+
+		if (!directionSize)
+			triOffSetSize += triOffSetSizeIncriment;
+		else
+			triOffSetSize -= triOffSetSizeIncriment;
+
+		if (triOffSetSize > triOffSetSizeMax || triOffSetSize < triOffSetSizeMin)
+			directionSize = !directionSize;
+
+		GLint uniformModel = glGetUniformLocation(shaderProgram, "model");
+		GLint uniformProjection = glGetUniformLocation(shaderProgram, "projection");
+		glm::mat4 model(1.0f);
+
+		model = glm::translate(model, glm::vec3(triOffSetX, triOffSetY, -2.5f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
+		model = glm::rotate(model, triCurrentAngle * toRadians, glm::vec3(0.7f, 0.5f, 1.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+
+
+		// desenhando o triangulo
+		glUseProgram(shaderProgram); // programa que queremos desenhar
+
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3); //Triangulo começando na posição 0, numero de pontos 3
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+			glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
+
+
+		//glBindVertexArray(VAO);
+		//// estamos usando o VAO
+		//glDrawArrays(GL_TRIANGLES, 0, 3); // trinagulo começando na posição 0 do VAO, e o número de pontos são 3
+		//glBindVertexArray(0);
+		// não estamos mais usando o VAO
 
 		glfwSwapBuffers(window);
 	}
